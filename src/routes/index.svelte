36f563efe5c5e070416ccb4d45cbca6e2a9e9ce0
ShadiestGoat <48590492+ShadiestGoat@users.noreply.github.com>
2022-06-09 17:59:36 +0100

    Ws fix

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -55,7 +55,7 @@
             await updateStake(true)
         }
 
-        const ws = new WebSocket(`${API_PROTCOL != "https" ? "ws" : "wss"}://${API_BASE_ORIGIN ? API_BASE_ORIGIN : location.host}/api/ws`)
+        const ws = new WebSocket(`${API_PROTCOL != "http" ? "wss" : "ws"}://${API_BASE_ORIGIN ? API_BASE_ORIGIN : location.host}/api/ws`)
 
         ws.onmessage = async function (e) {
             const eventData:{event:string, data:Record<string, unknown>} = JSON.parse(e.data)
